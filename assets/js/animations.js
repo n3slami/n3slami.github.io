656488e634a61df02b820bf59d1c105edc86cf76
n3slami <n3slami@gmail.com>
2024-08-18 23:36:53 -0400

    Add academic background section

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,6 +1,6 @@
 let observerOptions  = {
     root: null, // relative to document viewport
-    threshold: 0.2 // callback fires when 10% of the element is visible
+    threshold: 0.5 // callback fires when 10% of the element is visible
 };
 let observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -10,6 +10,22 @@ let observer = new IntersectionObserver((entries) => {
                     targets: entry.target,
                     opacity: [0, 1],
                     translateX: [-10, 0],
+                    duration: 1000
+                });
+            }
+            else if (entry.target.classList.contains("education-entry-odd")) {
+                anime({
+                    targets: entry.target,
+                    opacity: [0, 1],
+                    translateX: [-20, 0],
+                    duration: 1000
+                });
+            }
+            else if (entry.target.classList.contains("education-entry-even")) {
+                anime({
+                    targets: entry.target,
+                    opacity: [0, 1],
+                    translateX: [20, 0],
                     duration: 1000
                 });
             }
@@ -25,8 +41,28 @@ anime({
     duration: 1000,
     autoplay: false
 });
+anime({
+    targets: document.querySelectorAll(".education-entry-odd"),
+    opacity: [0, 1],
+    translateX: [-10, 0],
+    duration: 1000,
+    autoplay: false
+});
+anime({
+    targets: document.querySelectorAll(".education-entry-even"),
+    opacity: [0, 1],
+    translateX: [10, 0],
+    duration: 1000,
+    autoplay: false
+});
 
 document.querySelectorAll(".paper-card").forEach(element => {
+    observer.observe(element);
+});
+document.querySelectorAll(".education-entry-odd").forEach(element => {
+    observer.observe(element);
+});
+document.querySelectorAll(".education-entry-even").forEach(element => {
     observer.observe(element);
 });
 
